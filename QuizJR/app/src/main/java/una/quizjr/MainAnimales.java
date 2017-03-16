160522caf69fc8001373e1da20a4d144dd7941ac
asvaaron <asvaaron@gmail.com>
2017-03-15 19:48:16 -0600

    Empezando Animales

--- QuizJR/app/src/main/java/una/quizjr/MainAnimales.java
+++ QuizJR/app/src/main/java/una/quizjr/MainAnimales.java
@@ -1,18 +1,48 @@
 package una.quizjr;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
+
+import java.util.concurrent.ThreadLocalRandom;
 
 public class MainAnimales extends AppCompatActivity {
-
+    private String [] animales={"cow","dog","monkey","pig"};
+    int id []={R.drawable.cow,R.drawable.dog,R.drawable.monkey,R.drawable.pig};
+    private ImageSwitcher sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_animales);
         this.setActionBar("Animales");
+
+        sw= (ImageSwitcher) findViewById(R.id.imageswitcher);        sw.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView (){
+                ImageView imageview = new ImageView(getApplicationContext());
+                imageview.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                return imageview;
+
+            }
+
+        });
+
     }
 
-    public void setActionBar(String msg){
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void change ()
+    {   int randomNum = ThreadLocalRandom.current().nextInt(3 + 1, 0);
+
+
+    }
+
+    private void setActionBar(String msg){
         getSupportActionBar().setTitle(msg);
     }
 }
